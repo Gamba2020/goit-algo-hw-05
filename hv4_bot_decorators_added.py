fd01bb210a92e3ecd0d1c24a9dc72fb4bd702abe
Gamba <dmitriyi.nikitiuk@gmail.com>
2024-05-07 00:22:11 +0300

    ver 1

--- hv4_bot_decorators_added.py
+++ hv4_bot_decorators_added.py
@@ -12,7 +12,6 @@ def input_error(func):
             return "Wrong format of argument"
     return inner
 
-@input_error
 def parse_input(user_input):
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
